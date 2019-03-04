5eaa234decd606da3652de5393c057f0c4240786
Oksana Gorin <oksana_gorin@yahoo.com>
2019-03-04 10:19:44 +0100

    first change

--- lesGIT/Controllers/HomeController.cs
+++ lesGIT/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace lesGIT.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.!!!!!!!!!!!!!!! GIT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
 
             return View();
         }
